d9f6ef3befed9ec22ce08272f49f0278a71e9112
Ross Shoger <ross.shoger@usnr.com>
2019-11-26 11:42:17 -0800

    perform all steps in setup

--- create-venv.ps1
+++ create-venv.ps1
@@ -1,5 +1,10 @@
 Param ($OutPath = "third_party")
 
+if (Test-Path ./.venv) {
+  Write-Host "Cleaning existing Virtualenv (.venv).." -ForegroundColor Magenta
+  rm -r -fo .venv
+}
+
 Write-Host "Installing Virtualenv.." -ForegroundColor Magenta
 
 pip install -U pip
@@ -15,7 +20,19 @@ Write-Host "Creating a Virtualenv.." -ForegroundColor Magenta
 
 python -m virtualenv --python="$pythonPath" .venv
 
-Write-Host "`nTo complete installation, run:`n"
-Write-Host " . .\.venv\Scripts\activate" -ForegroundColor Cyan
+Write-Host "Activating Virtualenv.." -ForegroundColor Magenta
+
+. .\.venv\Scripts\activate
+
+Write-Host "Installing pip packages.." -ForegroundColor Magenta
+
+python -m pip install -r requirements.txt
+
+deactivate
+
+Write-Host "`nTo activate environmentn:`n" -ForegroundColor Yellow
+Write-Host " . .\.venv\Scripts\activate`n" -ForegroundColor Cyan
+
+Write-Host "`nTo re-install pip packages:`n" -ForegroundColor Yellow
 Write-Host " pip install -r requirements.txt`n" -ForegroundColor Cyan
 
--- download-python.ps1
+++ download-python.ps1
@@ -7,26 +7,32 @@ if (!(Test-Path $OutPath)) {
   mkdir -Force "$OutPath/python" | Out-Null
 }
 
-$result = Invoke-WebRequest -method GET -Uri $PythonDownloadURL -Headers $resourceHeaders
+# Perform an http GET for the file.
+$result      = Invoke-WebRequest -method GET -Uri $PythonDownloadURL -Headers $resourceHeaders
 
-$outFilePath = "$PWD\"+$OutPath+"\python\"+$result.Headers.'Content-Disposition'.Split("=")[1]
+# Determine the file name to use.
+$outFilePath = "$PWD\"+$OutPath+"\"+$result.Headers.'Content-Disposition'.Split("=")[1]
 
-Write-Progress -Activity "Unpacking Python" -Status "0% Complete:" -PercentComplete 0
+#
+# Download WinPython
+#
+Write-Progress -Activity "Configuring Python" -CurrentOperation "Downloading Python" -Status "0% Complete:" -PercentComplete 0
 
 [System.IO.File]::WriteAllBytes($outFilePath, $result.content)
 
-Write-Progress -Activity "Unpacking Python" -Status "50% Complete:" -PercentComplete 50 
+Write-Progress -Activity "Configuring Python" -CurrentOperation "Unpacking Python" -Status "50% Complete:" -PercentComplete 50 
 
-#7z x -o"$OutPath" -bso0 -bsp0 -y $outFilePath
-& $outFilePath -bso0 -bsp0 -y -o"$OutPath" "$outFilePath" | Out-Null
+& $outFilePath -bso0 -bsp0 -y -o"$OutPath/python" "$outFilePath" | Out-Null
+
+Write-Progress -Activity "Configuring Python" -CurrentOperation "Finding Python Path" -Status "75% Complete:" -PercentComplete 75 
+
+#
+# Resolve path to python.
+#
+$result = (Get-ChildItem -Path "$OutPath/python" -Recurse -Include python.exe | Where-Object { (Split-Path $_.Directory -Leaf) -match 64 }).FullName
 
 Write-Progress -Activity "Unpacking Python" -Status "100% Compete" -Completed
 
-#$pythonPath = ((& 7z l $outFilePath | where-object {$_ -match "64\\python.exe"}) -split '\s+?\b')[3]
-
-$pythonPath = (Get-ChildItem -Path "$OutPath/python" -Recurse -Include python.exe | Where-Object { (Split-Path $_ -Leaf) -match 64 }).DirectoryName
-
-$result = "$PWD\$OutPath\$pythonPath"
-
-return $result
+# Return a path to python.
+return "$result"
 
